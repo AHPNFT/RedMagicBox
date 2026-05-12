85c2d2ce6d86761162da4fbc139c2d85fa0b2f3b
aibgsps-sys <497200876@qq.com>
2026-05-12 22:46:25 +0800

    v3.9.5: APK download URL fixed to latest link

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -4,7 +4,7 @@ import { checkRoot } from '../utils/rootDetection';
 import { t } from '../i18n';
 
 const SPLASH_IMG = require('../assets/hongmo.jpg');
-const APP_VERSION = '3.9.4';
+const APP_VERSION = '3.9.5';
 
 interface SplashScreenProps {
   onFinish: (rooted: boolean) => void;
