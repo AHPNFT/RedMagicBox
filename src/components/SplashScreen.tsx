53c546bf2e0f3b2a04437bb945a6e0a8b6273f10
aibgsps-sys <497200876@qq.com>
2026-05-15 00:21:45 +0800

    v3.9.20: fix activation code seed mismatch + new testnet contract with MockRMAB

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -4,7 +4,7 @@ import { checkRoot } from '../utils/rootDetection';
 import { t } from '../i18n';
 
 const SPLASH_IMG = require('../assets/hongmo.jpg');
-const APP_VERSION = '3.9.19';
+const APP_VERSION = '3.9.20';
 
 interface SplashScreenProps {
   onFinish: (rooted: boolean) => void;
